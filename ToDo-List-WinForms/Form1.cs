cc09d7e8b4c6a11ad5963912a7241538d541135d
DeepJ786 <112116451+DeepJ786@users.noreply.github.com>
2023-04-30 17:47:29 +0100

    Add project files.

---
+++ ToDo-List-WinForms/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo_List_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to the ToDo List!\n>To add a task, click on the textbox at the top left, then click the plus button\n>To indicate a completed task, check the box next to the task\n>To remove a task, select it and click on the remove button at the bottom.", "BOO!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = listBox1.Items.Count - 1; i >= 0; i--)
+            {
+                 listBox1.Items.RemoveAt(i);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
